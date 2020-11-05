b65904b4c87d8482401f61cbedab4488804b6b42
RobinGoedjen <mail@robingoedjen.xyz>
2020-11-05 09:45:53 +0100

    Added basic activity switch

--- app/src/main/java/com/example/osrstracker/MainActivity.java
+++ app/src/main/java/com/example/osrstracker/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.osrstracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.Console;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userNameEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userNameEdit = (EditText) findViewById(R.id.editTextPersonName);
+        userNameEdit.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    setContentView(R.layout.activity_statistics);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+
+
     }
 }
